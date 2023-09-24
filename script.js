5682875d2b186664350fd39aab87eeaaee2e8c19
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-24 15:30:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.addEventListener("DOMContentLoaded", () => {
   const emojiButtons = document.querySelectorAll(".emoji-button");
   const emojiContainer = document.querySelector(".emoji-container");
 
+  emojiButtons.forEach((button) => {
